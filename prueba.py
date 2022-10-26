41aced4f0a24074a227a612a9ecbb7782a44e52d
David Sanz <dsanzfuertes.15@gmail.com>
2022-10-26 12:13:03 +0200

    tabla hash terminada

--- prueba.py
+++
@@ -1,9 +0,0 @@
-
-
-def crear_tabla(tamanio):
-    """Crear una tabla hash vacia"""
-    tabla = [None]*tamanio
-    return tabla
-
-def cantidad_elementos(tabla):
-    """Devuelve la cantidad de elementos en la tabla"""q
